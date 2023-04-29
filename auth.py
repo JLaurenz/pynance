b133fbbb0003fd800aad23e6bd417c65ae898c39
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2023-04-29 20:50:56 +0800

    comit

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
 import pyrebase
+import streamlit as st
 import json
 
 firebaseConfig = {
@@ -18,7 +19,30 @@ auth = firebase.auth()
 db = firebase.database()
 storage = firebase.storage()
 
-choice = st.sidebar.selectbox("Menu", ["Login", "Signup"])
+def main():
+    st.set_page_config(page_title="PyNance")
+    st.title("PyNance")
+    st.header("Login")
+    col1, col2 = st.columns(2)
+    with col1:
+        email = st.text_input("Enter your email: ")
+    with col2:
+        password = st.text_input("Enter your password: ", type="password")
+    choice = st.radio("Login or Signup", ("Login", "Signup"))
+    button = st.button(choice)
 
-email = st.sidebar.text_input("Email")
-password = st.sidebar.text_input("Password", type='password')
+if button:
+    if choice == "Login":
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            st.success("Logged in as {}".format(email))
+            st.balloons()
+        except:
+            st.error("Invalid email or password")
+    elif choice == "Signup":
+        try:
+            auth.create_user_with_email_and_password(email, password)
+            st.success("Successfully created account for {}".format(email))
+        except:
+            st.error("Email already exists")
+
--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
-import pyrebase
-import json
-
-firebaseConfig = {
-  'apiKey': "AIzaSyBj7haZR97aCD-gpDgdS71WZ3EctxGuS6o",
-  'authDomain': "pynance-8f282.firebaseapp.com",
-  'projectId': "pynance-8f282",
-  'databaseURL': 'https://pynance-8f282-default-rtdb.firebaseio.com/',
-  'storageBucket': "pynance-8f282.appspot.com",
-  'messagingSenderId': "477676443144",
-  'appId': "1:477676443144:web:c8da38d005e2715238e3bd",
-  'measurementId': "G-QC5GTZ0KS8"
-}
+import auth.py as auth
 
 st.set_page_config(
     page_title="PyNance")
 
 st.title("PyNance")
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
-
-db = firebase.database()
-storage = firebase.storage()
-
-choice = st.sidebar.selectbox("Menu", ["Login", "Signup"])
-
-email = st.sidebar.text_input("Email")
-password = st.sidebar.text_input("Password", type='password')
+if st.button("Login"):
+    auth.main()
 
 st.header("**Monthly Income**")
 st.subheader("Salary")
