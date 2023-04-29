180b41680361492a213436c60d6962f22fbf4f9f
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2023-04-29 20:57:47 +0800

    renamed

---
+++ log.py
@@ -0,0 +1,22 @@
+import pyrebase
+import streamlit as st
+import json
+
+firebaseConfig = {
+  'apiKey': "AIzaSyBj7haZR97aCD-gpDgdS71WZ3EctxGuS6o",
+  'authDomain': "pynance-8f282.firebaseapp.com",
+  'projectId': "pynance-8f282",
+  'databaseURL': 'https://pynance-8f282-default-rtdb.firebaseio.com/',
+  'storageBucket': "pynance-8f282.appspot.com",
+  'messagingSenderId': "477676443144",
+  'appId': "1:477676443144:web:c8da38d005e2715238e3bd",
+  'measurementId': "G-QC5GTZ0KS8"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+db = firebase.database()
+storage = firebase.storage()
+
+
