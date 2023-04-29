4c1d4d026c3ed019f5e324ce9c97779a4643187a
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2023-04-29 21:14:51 +0800

    test2

--- log.py
+++ log.py
@@ -21,10 +21,35 @@ storage = firebase.storage()
 
 
 # When this method is called, it will create a new user in the database
-# with the given email and password
-def signup(email, password):
-    try:
-        user = auth.create_user_with_email_and_password(email, password)
-        return user
-    except:
-        return None
+def signup():
+    st.subheader("Create New Account")
+    new_email = st.text_input("Email")
+    new_password = st.text_input("Password", type="password")
+    confirm_password = st.text_input("Confirm Password", type="password")
+    if new_password == confirm_password:
+        if st.button("Signup"):
+            try:
+                user = auth.create_user_with_email_and_password(new_email, new_password)
+                st.success("You have successfully created a valid account")
+                st.info("Go to Login Menu to login")
+            except:
+                st.error("Oops! Something went wrong...")
+    else:
+        st.warning("Passwords do not match")
+
+
+# When this method is called, it will log the user in
+def login():
+    st.subheader("Login")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            st.success("You have successfully logged in")
+            with open('user.json', 'w') as f:
+                json.dump(user, f)
+            st.info("Proceed to Prediction Menu")
+        except:
+            st.error("Oops! Something went wrong...")
+
