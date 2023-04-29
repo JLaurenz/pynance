ca3fef3a805382edd5e48b4b569d4044c0c6e217
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2023-04-29 21:14:56 +0800

    tests

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
 import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
-from log import db, auth, signup
+from log import db, auth, signup, login
 
 # set choice as global variable
 def main():
     st.set_page_config(page_title="PyNance")
     st.title("PyNance")
-    st.header("Login")
-    col1, col2 = st.columns(2)
-    with col1:
-        email = st.text_input("Enter your email: ")
-    with col2:
-        password = st.text_input("Enter your password: ", type="password")
-    choice = st.radio("Login or Signup", ("Login", "Signup"))
-    button = st.button(choice)
-
-    if button:
-        if choice == "Login":
-            try:
-                user = auth.sign_in_with_email_and_password(email, password)
-                st.success("Logged in as {}".format(user["email"]))
-            except:
-                st.error("Invalid email or password")
-        if choice == "Signup":
-            if signup(email, password):
-                st.success("Account created for {}".format(email))
-            else:
-                st.error("Account already exists")
-    else:
-        try:
-            user = auth.create_user_with_email_and_password(email, password)
-            st.success("Account created for {}".format(user["email"]))
-        except:
-            st.error("Account already exists")
-
+    # Login
+    st.sidebar.title("Login")
+    st.sidebar.subheader("Login to your account")
+    # call log login function
+    login()
+    # Signup
+    st.sidebar.title("Signup")
+    st.sidebar.subheader("Create an account")
+    # call log signup function
+    signup()
+    
 if __name__ == "__main__":
     main()
